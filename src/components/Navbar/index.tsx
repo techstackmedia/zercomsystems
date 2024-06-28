ae727de8b1811e72f78f68ccf3bcaf1839305949
Osagie Bello <info@techstackmedia.com>
2024-06-28 02:48:39 +0100

    feat: add logo to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './index.module.css';
+import logo from '../../../public/assets/logo.png'
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-          <Link href='/'>SINC PARTNERS</Link>
-      </div>
-      <div className={styles.links}>
+          <Link href='/' className={styles.sinc}><Image src={logo} width={114.94} height={64} alt='sinc logo' /></Link>
+          <div className={styles.navLinks}>
+          <div className={styles.links}>
         <Link href="/about">About</Link>
         <Link href="/sip">SIP</Link>
         <Link href="/studio">Studio</Link>
@@ -16,7 +18,10 @@ const Navbar: React.FC = () => {
         <Link href="/platforms">Platforms</Link>
         <Link href="/initiatives">Initiatives</Link>
         <Link href="/more">More</Link>
+        </div>
       </div>
+      </div>
+      
       <div className={styles.buttons}>
         <Link href="/sync" className={styles.syncButton}>SINC With Us</Link>
         <Link href="/apply" className={styles.applyButton}>Apply to SIP 1.0</Link>
